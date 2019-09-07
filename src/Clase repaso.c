c8f331630e2b662916d7410b825299179cae9438
alumno <alumno@lab6pc00>
2019-09-07 11:53:49 -0300

    Claserepaso5

---
+++ src/Clase repaso.c
@@ -0,0 +1,120 @@
+/*
+ ============================================================================
+ Name        : Clase.c
+ Author      : 
+ Version     :
+ Copyright   : GLP 3
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+#define CANTIDAD 3
+
+void mostrarUnArray(int parametroArray[], int cant);
+void inicializarUnArray(int parametroArray[], int cant);
+void cargaSecuencialUnArray(int parametroArray[], int cant, char mensaje());
+int dameElMaximo(int parametroArray[], int cant);
+int dameElIndiceDelMaximo(int parametroArray[], int cant);
+void ordenarDesc(int parametroArray[], int cant);
+void ordenarAsc(int parametroArray[], int cant);
+
+int main ()
+{
+	int miArray[CANTIDAD]; // direccion de memoria donde empezamos a escribir
+
+	inicializarUnArray(miArray,CANTIDAD -1);
+	cargaSecuencialUnArray(miArray,CANTIDAD);
+	cargaSecuencialUnArray(miArray,CANTIDAD, "Ingrese una edad");
+	cargaSecuencialUnArray(miArray,CANTIDAD, "Ingrese una altura");
+	cargaSecuencialUnArray(miArray,CANTIDAD, "Ingrese temperatura");
+	mostrarUnArray(miArray,CANTIDAD);
+	int maximo = dameElMaximo(miArray,CANTIDAD);
+	printf("maximo: %d",maximo);
+	int indiceMaximo = dameElIndiceDelMaximo(miArray,CANTIDAD);
+	printf("maximo: %d",indiceMaximo);
+	return 0;
+}
+
+
+void mostrarUnArray(int parametroArray[], int cant)
+{
+	int indice;
+
+	for(indice = 0;indice<CANTIDAD;indice++)
+	{
+
+	printf("\n numero :%d", parametroArray[indice]);
+
+	}
+
+}
+void inicializarUnArray(int parametroArray[], int cant)
+{
+
+	int indice;
+
+	for (indice = 0;indice<CANTIDAD;indice++)
+		{
+			parametroArray[indice]=0;
+
+		}
+}
+void cargaSecuencialUnArray(int parametroArray[], int cant, char mensaje())
+{
+
+	int indice;
+
+	for(indice = 0;indice<CANTIDAD;indice++)
+	{
+			printf("\n %s", mensaje);
+			scanf("%d",&parametroArray[indice]);
+	}
+
+}
+
+int dameElMaximo(int parametroArray[], int cant)
+{
+	int indice;
+	int maximo;
+
+	for(indice=0;indice<cant;indice++)
+	{
+		if(indice == 0 || parametroArray[indice]>maximo)
+		{
+			maximo=parametroArray[indice];
+
+		}
+	}
+	return maximo;
+
+}
+
+int dameElIndiceDelMaximo(int parametroArray[], int cant)
+{
+		int indice;
+		int indiceDelMaximo;
+		int maximo;
+
+		for(indice=0;indice<cant;indice++)
+		{
+			if(indice == 0 || parametroArray[indice]>maximo)
+			{
+				maximo=parametroArray[indice];
+				indiceDelMaximo=indice;
+
+			}
+		}
+		return indiceDelMaximo;
+}
+
+void ordenarDesc(int parametroArray[], int cant){
+	int indice;
+
+	for (indice = 0;indice<cant;indice++){
+
+	}
+}
+
